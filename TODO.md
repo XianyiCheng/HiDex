101a4fd734a8ddb0bba4706c2ad6312edbf209d9
xianyi <chengxy0418@gmail.com>
2022-09-27 16:36:05 -0400

    add minor comments, next steps

--- TODO.md
+++ TODO.md
@@ -1,1 +1,3 @@
-- [ ] Debug run line by line
+- [ ] Go through the code. And write down the main template reference for each class: Tree, Node, Level1Tree, template for Task and State in "Design.md".
+
+- [ ] Try to create a Task that do object motion generation through contact modes. You should not change Tree, Node, Level1Tree to do this task.
--- tests/test_level1.cpp
+++ tests/test_level1.cpp
@@ -1,18 +1,18 @@
 // #include "../search/base.h"
 #include "../search/level1.h"
 
-#include "../tasks/constrained_manipulation.h"
+#include "../tasks/test_task_l1.h"
 
 #include "../mechanics/utilities/utilities.h"
 // this header file includes ObjectState (State), and
-// ConstrainedManipulationTask (Task)
+// TestTaskL1 (Task)
 
 int main() {
 
   // Test it with an example of planning to push an object in the plane
 
-  std::shared_ptr<ConstrainedManipulationTask> task =
-      std::make_shared<ConstrainedManipulationTask>(); // std::make_shared
+  std::shared_ptr<TestTaskL1> task =
+      std::make_shared<TestTaskL1>(); // std::make_shared
   // define the task
   // environment, object, world model: collision detection ...
   // ...
@@ -24,20 +24,20 @@ int main() {
   goal_object_pose << 1, 0, 0, 0, 0, 0, 1;
 
   task->initialize(start_object_pose, goal_object_pose,
-                   ConstrainedManipulationTask::SearchOptions(
+                   TestTaskL1::SearchOptions(
                        Eigen::Vector3d(-1, -1, -1), Eigen::Vector3d(2, 2, 2)));
 
   // generate start_state from task
-  ConstrainedManipulationTask::State start_state = task->get_start_state();
+  TestTaskL1::State start_state = task->get_start_state();
 
-  HMP::Level1Tree<ConstrainedManipulationTask::State,
-                  ConstrainedManipulationTask>
+  HMP::Level1Tree<TestTaskL1::State,
+                  TestTaskL1>
       tree(task, start_state);
 
   HMP::ComputeOptions compute_options;
   compute_options.max_iterations = 100;
 
-  HMP::Node<ConstrainedManipulationTask::State> *current_node =
+  HMP::Node<TestTaskL1::State> *current_node =
       tree.m_root_node.get();
 
   while (!tree.is_terminal(current_node)) {
