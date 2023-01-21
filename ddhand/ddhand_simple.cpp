b51b39f39595620077040869fb584af52a29b54e
xianyi <chengxy0418@gmail.com>
2023-01-20 19:29:28 -0500

    ddhand data saver

--- ddhand/ddhand_simple.cpp
+++ ddhand/ddhand_simple.cpp
@@ -19,6 +19,143 @@
 #include "../mechanics/utilities/io.h"
 #include "../tasks/visualization.h"
 
+template <class State, class State2, class Task>
+MatrixXd get_ddhand_output(const std::vector<State> &object_trajectory,
+                           const std::vector<State2> &action_trajectory,
+                           std::shared_ptr<Task> task, std::string para_path) {
+
+  YAML::Node config = YAML::LoadFile(para_path);
+  double hand_x = config["hand_position"]["x"].as<double>();
+  double hand_y = config["hand_position"]["y"].as<double>();
+  double hand_z = config["hand_position"]["z"].as<double>();
+  DartDDHand *hand = new DartDDHand(L_FINGER);
+  Vector7d handx;
+  handx << hand_x, hand_y, hand_z, 0, 0, 0, 1;
+
+  hand->setHandFrameTransform(handx);
+  std::vector<VectorXd> output;
+  int t_span = 10;
+
+  for (int i = 1; i < action_trajectory.size(); ++i) {
+    {
+      VectorXd output_row(4 + 6 * task->number_of_robot_contacts + 7);
+      output_row.setZero();
+      output.push_back(output_row);
+    }
+    int t = action_trajectory[i].timestep;
+    int t_next;
+    if (i == action_trajectory.size() - 1) {
+      t_next = object_trajectory.size() - 1;
+    } else {
+      t_next = action_trajectory[i + 1].timestep;
+    }
+
+    VectorXd mnp_config = task->get_robot_config_from_action_idx(
+        action_trajectory[i].finger_index);
+
+    // get the array of object pose
+    std::vector<Vector7d> object_poses;
+    if (t_next <= t) {
+      for (int kk = 0; kk < t_span; ++kk) {
+        object_poses.push_back(object_trajectory[t].m_pose);
+      }
+    } else {
+      std::vector<Vector7d> object_poses_all;
+      // object_poses_all.push_back(object_trajectory[t].m_pose);
+      for (int kk = t + 1; kk <= t_next; ++kk) {
+        object_poses_all.insert(object_poses_all.end(),
+                                object_trajectory[kk].m_path.begin(),
+                                object_trajectory[kk].m_path.end());
+      }
+      object_poses = object_poses_all;
+      // int n_span = std::max(int(object_poses_all.size())- 2, 0) / 3;
+      // object_poses.push_back(object_poses_all[0]);
+      // object_poses.push_back(object_poses_all[n_span]);
+      // object_poses.push_back(object_poses_all[2 * n_span]);
+      // object_poses.push_back(object_poses_all[3 * n_span]);
+      // object_poses.push_back(object_poses_all[object_poses_all.size() - 1]);
+    }
+
+    // for each object pose, get the array of mnp config
+
+    for (auto x : object_poses) {
+      VectorXd mnp_config_world(6 * task->number_of_robot_contacts);
+      for (int n_pt = 0; n_pt < task->number_of_robot_contacts; ++n_pt) {
+        if (std::isnan(mnp_config[6 * n_pt])) {
+          mnp_config_world.segment(6 * n_pt, 6) =
+              mnp_config.segment(6 * n_pt, 6);
+          mnp_config_world(6 * n_pt) = -7777;
+          mnp_config_world(6 * n_pt + 1) = -7777;
+          mnp_config_world(6 * n_pt + 2) = -7777;
+          continue;
+        }
+        Vector3d p = mnp_config.segment(6 * n_pt, 3);
+        Vector3d n = mnp_config.segment(6 * n_pt + 3, 3);
+        Eigen::Matrix3d R;
+        R = quat2SO3(x(6), x(3), x(4), x(5));
+        Vector3d p_world = R * p + x.segment(0, 3);
+        Vector3d n_world = R * n;
+        mnp_config_world.segment(6 * n_pt, 3) = p_world;
+        mnp_config_world.segment(6 * n_pt + 3, 3) = n_world;
+      }
+
+      VectorXd output_row(4 + 6 * task->number_of_robot_contacts + 7);
+
+      output_row.segment(0, 4) =
+          hand->getHandFrameConfig(mnp_config, x);
+      output_row.segment(4, 6 * task->number_of_robot_contacts) =
+          mnp_config_world;
+      output_row.segment(4 + 6 * task->number_of_robot_contacts, 7) = x;
+
+      output.push_back(output_row);
+    }
+  }
+
+  MatrixXd output_mat(output.size(),4 + 6 * task->number_of_robot_contacts + 7);
+  for (int i = 0; i < output.size(); ++i) {
+    output_mat.row(i) = output[i];
+  }
+  return output_mat;
+}
+
+void visualize_output_file(std::shared_ptr<WorldTemplate> world,
+                           std::string file_name) {
+  MatrixXd data = openData(file_name);
+  int n_data = data.rows();
+  int n_pts = (data.cols() - 7) / 6;
+  std::vector<Vector7d> object_traj;
+  std::vector<VectorXd> mnp_traj;
+  for (int i = 0; i < n_data; ++i) {
+    // if all elements are zero, skip this row
+    if (data.row(i).sum() == 0) {
+      continue;
+    }
+    VectorXd mnp_config_world = data.row(i).segment(4, 6 * n_pts);
+    VectorXd mnp_config(6 * n_pts);
+    Vector7d object_pose = data.row(i).segment(4 + 6 * n_pts, 7);
+
+    Matrix3d R = quat2SO3(object_pose(6), object_pose(3), object_pose(4),
+                          object_pose(5));
+    Matrix3d R_inv = R.transpose();
+    Vector3d t = object_pose.segment(0, 3);
+    for (int j = 0; j < n_pts; ++j) {
+      if (std::isnan(mnp_config_world[6 * j])) {
+        mnp_config.segment(6 * j, 6) = mnp_config_world.segment(6 * j, 6);
+        continue;
+      }
+      Vector3d pw = mnp_config_world.segment(6 * j, 3);
+      Vector3d nw = mnp_config_world.segment(6 * j + 3, 3);
+      Vector3d p = R_inv * (pw - t);
+      Vector3d n = R_inv * nw;
+      mnp_config.segment(6 * j, 3) = p;
+      mnp_config.segment(6 * j + 3, 3) = n;
+    }
+    object_traj.push_back(object_pose);
+    mnp_traj.push_back(mnp_config);
+  }
+  world->setPlaybackTrajectory(object_traj, mnp_traj);
+}
+
 const CMGTASK::State2::Action CMGTASK::State2::no_action =
     CMGTASK::State2::Action(-1, -1);
 const CMGTASK::State::Action CMGTASK::State::no_action = -1;
@@ -107,10 +244,12 @@ void setup(std::shared_ptr<CMGTASK> task) {
       config["start_pose"]["z"].as<double>() - box_lz / 2;
 
   rrt_options.eps_trans = config["rrt_options"]["eps_trans"].as<double>();
-  rrt_options.eps_angle = (3.14/180.0)*config["rrt_options"]["eps_angle_deg"].as<double>();
+  rrt_options.eps_angle =
+      (3.14 / 180.0) * config["rrt_options"]["eps_angle_deg"].as<double>();
   rrt_options.max_samples = config["rrt_options"]["max_samples"].as<int>();
 
-  rrt_options.goal_biased_prob = config["rrt_options"]["goal_biased_prob"].as<double>();
+  rrt_options.goal_biased_prob =
+      config["rrt_options"]["goal_biased_prob"].as<double>();
 
   bool is_refine = false;
   double refine_dist = 0.1;
@@ -132,9 +271,9 @@ void setup(std::shared_ptr<CMGTASK> task) {
 
     Vector3d pos;
     pos << v(0) * box_lx / 2, v(1) * box_ly / 2, v(2) * box_lz / 2;
-    if (abs(v[0]) > 0.8 || abs(v[1] > 0.8) ){
-      continue;
-    }
+    // if (abs(v[0]) > 0.8 || abs(v[1] > 0.8)) {
+    //   continue;
+    // }
     ContactPoint p(pos, -v.tail(3));
     surface_pts.push_back(p);
   }
@@ -161,6 +300,7 @@ int main(int argc, char *argv[]) {
   // output_file = task_name + "_ouput.csv";
   output_file = "output.csv";
 
+  std::string setup_path = std::string(SRC_DIR) + "/data/ddhand/simple_setup.yaml";
   setup(task);
   // change the above code using case statement
 
@@ -171,11 +311,11 @@ int main(int argc, char *argv[]) {
 
   int random_seed = config["random_seed"].as<int>();
 
-  // if (visualize_option == "csv") {
-  //   visualize_output_file(task->m_world, output_file_path);
-  //   task->m_world->startWindow(&argc, argv);
-  //   return 0;
-  // }
+  if (visualize_option == "csv") {
+    visualize_output_file(task->m_world, output_file_path);
+    task->m_world->startWindow(&argc, argv);
+    return 0;
+  }
 
   if (visualize_option == "setup") {
     VisualizeSG(task->m_world, task->start_object_pose, task->goal_object_pose);
@@ -231,8 +371,9 @@ int main(int argc, char *argv[]) {
   VisualizeStateTraj(task->m_world, task, object_trajectory, action_trajectory);
 
   // std::remove(output_file_path.c_str());
-  // MatrixXd output_mat = get_output(object_trajectory, action_trajectory,
-  // task); saveData(output_file_path, output_mat);
+  MatrixXd output_mat =
+      get_ddhand_output(object_trajectory, action_trajectory, task, setup_path);
+  saveData(output_file_path, output_mat);
 
   std::cout << "Total level 1 tree nodes " << tree.count_total_nodes()
             << std::endl;
